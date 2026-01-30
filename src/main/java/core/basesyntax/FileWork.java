8923de23db24131540771a8b192c161068d79b8c
mc704ed <mykyta.churylov@student.tuke.sk>
2026-01-30 13:56:27 +0100

    update

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        String[] strings = new String[300];
+        String[] result = new String[strings.length];
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            if (file.length() == 0) {
+                return new String[0];
+            }
+            StringBuilder builder = new StringBuilder();
+            int value = bufferedReader.read();
+            int count = 0;
+            int count1 = 0;
+            while(value != -1){
+                while(value != 32 && value != -1){
+                    builder.append((char)value);
+                    value = bufferedReader.read();
+                }
+                if (builder.length() > 0){
+                    strings[count++] = builder.toString();
+                    builder.setLength(0);
+                }
+                value = bufferedReader.read();
+            }
+            for (String string : strings) {
+                if (string != null && !string.isEmpty()) {
+                    if (string.charAt(0) == 'w' ||  string.charAt(0) == 'W') {
+                        result[count1++] = string.toLowerCase();
+                    }
+                }
+            }
+            return Arrays.copyOf(result, count1);
+        } catch (IOException e) {
+            throw new RuntimeException("cant read file ",e);
+        }
     }
 }
